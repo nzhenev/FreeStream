83d66ab8c48182adcecd13ce4673aa5029b5e072
Daethyra <109057945+Daethyra@users.noreply.github.com>
2024-03-30 21:11:49 -0700

    feat(chatbot): add general purpose chatbot that doesn't require file uploading
    - Added a new file "2_💬_Chatbot.py" to incorporate a chatbot feature into the FreeStream application. The chatbot allows users to interact with various AI models, including GPT-3.5 Turbo, GPT-4 Turbo, Claude: Haiku, Claude: Sonnet, Claude: Opus, and Gemini-Pro. Users can select a chat model, engage in conversations, and view conversation history. Additionally, the chatbot supports dynamic loading of selected models and includes a temperature slider for adjusting model responses.
    
    This commit introduces chatbot functionality to enhance user interaction within the FreeStream application, providing a diverse range of AI models for engaging conversations and enabling users to explore different conversational experiences. It also contains an experimental background, a GIF of anime rain falling.

---
+++ freestream/pages/2_💬_Chatbot.py
@@ -0,0 +1,186 @@
+import os
+
+import streamlit as st
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain_anthropic import ChatAnthropic
+from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import ChatOpenAI
+from pages import (
+    PrintRetrievalHandler,
+    RetrieveDocuments,
+    StreamHandler,
+    footer,
+    set_bg_local,
+    set_llm,
+)
+
+# Initialize LangSmith tracing
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = "FreeStream-v4.0.0"
+os.environ["LANGCHAIN_ENDPOINT"] = st.secrets.LANGCHAIN.LANGCHAIN_ENDPOINT
+os.environ["LANGCHAIN_API_KEY"] = st.secrets.LANGCHAIN.LANGCHAIN_API_KEY
+
+# Set up page config
+st.set_page_config(page_title="FreeStream: Chatbot", page_icon="💬")
+st.title("💬Chatbot")
+st.header(":green[_General Use Chatbot_]", divider="red")
+st.caption(":violet[_Ask Your Chatbot Questions_]")
+# Show footer
+st.markdown(footer, unsafe_allow_html=True)
+
+# Add sidebar
+st.sidebar.subheader("__User Panel__")
+
+# Create a on/off switch for the GIF background
+gif_bg = st.sidebar.toggle(
+    label="Rain Background",
+    value=False,
+    key="gif_background",
+    help="Turn on an experimental background.",
+)
+if gif_bg:
+    set_bg_local("assets/62.gif")
+
+# Add temperature header
+temperature_header = st.sidebar.markdown(
+    """
+    ## Temperature Slider
+    """
+)
+# Add the sidebar temperature slider
+temperature_slider = st.sidebar.slider(
+    label=""":orange[Set LLM Temperature]. The :blue[lower] the temperature, the :blue[less] random the model will be. The :blue[higher] the temperature, the :blue[more] random the model will be.""",
+    min_value=0.0,
+    max_value=1.0,
+    value=0.0,
+    step=0.05,
+    key="llm_temperature",
+)
+
+# Setup memory for contextual conversation
+msgs = StreamlitChatMessageHistory()
+memory = ConversationBufferMemory(
+    memory_key="chat_history", chat_memory=msgs, return_messages=True
+)
+
+# Create a dictionary with keys to chat model classes
+model_names = {
+    "GPT-3.5 Turbo": ChatOpenAI(  # Define a dictionary entry for the "ChatOpenAI GPT-3.5 Turbo" model
+        model="gpt-3.5-turbo-0125",  # Set the OpenAI model name
+        openai_api_key=st.secrets.OPENAI.openai_api_key,  # Set the OpenAI API key from the Streamlit secrets manager
+        temperature=temperature_slider,  # Set the temperature for the model's responses using the sidebar slider
+        streaming=True,  # Enable streaming responses for the model
+        max_tokens=4096,  # Set the maximum number of tokens for the model's responses
+        max_retries=1,  # Set the maximum number of retries for the model
+    ),
+    "GPT-4 Turbo": ChatOpenAI(
+        model="gpt-4-0125-preview",
+        openai_api_key=st.secrets.OPENAI.openai_api_key,
+        temperature=temperature_slider,
+        streaming=True,
+        max_tokens=4096,
+        max_retries=1,
+    ),
+    "Claude: Haiku": ChatAnthropic(
+        model="claude-3-haiku-20240307",
+        anthropic_api_key=st.secrets.ANTHROPIC.anthropic_api_key,
+        temperature=temperature_slider,
+        streaming=True,
+        max_tokens=4096,
+    ),
+    "Claude: Sonnet": ChatAnthropic(
+        model="claude-3-sonnet-20240229",
+        anthropic_api_key=st.secrets.ANTHROPIC.anthropic_api_key,
+        temperature=temperature_slider,
+        streaming=True,
+        max_tokens=4096,
+    ),
+    "Claude: Opus": ChatAnthropic(
+        model="claude-3-opus-20240229",
+        anthropic_api_key=st.secrets.ANTHROPIC.anthropic_api_key,
+        temperature=temperature_slider,
+        streaming=True,
+        max_tokens=4096,
+    ),
+    "Gemini-Pro": ChatGoogleGenerativeAI(
+        model="gemini-pro",
+        google_api_key=st.secrets.GOOGLE.google_api_key,
+        temperature=temperature_slider,
+        top_k=50,
+        top_p=0.7,
+        convert_system_message_to_human=True,
+        max_output_tokens=4096,
+        max_retries=1,
+    ),
+}
+
+# Create a dropdown menu for selecting a chat model
+selected_model = st.selectbox(
+    label="Choose your chat model:",  # Set the label for the dropdown menu
+    options=list(model_names.keys()),  # Set the available model options
+    key="model_selector",  # Set a unique key for the dropdown menu
+    on_change=lambda: set_llm(
+        st.session_state.model_selector, model_names
+    ),  # Set the callback function
+)
+
+# Load the selected model dynamically
+llm = model_names[
+    selected_model
+]  # Get the selected model from the `model_names` dictionary
+
+# Define the prompt template
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a general purpose AI chatbot who actively works to assist others in reality comprehension, exploration of curiosity, and the practice of critical thinking skills. Guide them to the right answer and only give direct advice when asked to do so.",
+        ),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("human", "{question}"),
+    ]
+)
+
+# Create a chain that ties everything together
+chain = prompt_template | llm
+chain_with_history = RunnableWithMessageHistory(
+    runnable=chain,
+    get_session_history=lambda session_id: msgs,
+    input_messages_key="question",
+    history_messages_key="chat_history",
+)
+
+### Chat History ###
+# if the length of messages is 0, or when the user \
+# clicks the clear button,
+# show a default message from the AI
+if len(msgs.messages) == 0 or st.sidebar.button("Clear message history"):
+    msgs.clear()
+
+# Display coversation history window
+avatars = {"human": "user", "ai": "assistant"}
+for msg in msgs.messages:
+    st.chat_message(avatars[msg.type]).write(msg.content)
+
+# Display user input field and enter button
+if user_query := st.chat_input(placeholder="Ask me anything!"):
+    st.chat_message("user").write(user_query)
+
+    # Display assistant response
+    # Using a `with` block instantly displays the response without having to `st.write` it
+    with st.chat_message("assistant"):
+        stream_handler = StreamHandler(st.empty())
+        response = chain_with_history.invoke(
+            {"question": user_query},
+            config={
+                "configurable": {"session_id": "any"},
+                "callbacks": [stream_handler],
+            },
+        )
+        # Force print Gemini's response
+        if selected_model == "Gemini-Pro":
+            st.write(response.content)
