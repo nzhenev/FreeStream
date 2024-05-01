d523c95c778d1cb6e05af13394e4907731be51c2
Daethyra <109057945+Daethyra@users.noreply.github.com>
2024-04-30 21:04:35 -0700

    Alphabetized chatbot list

--- freestream/🏡_Home.py
+++ freestream/🏡_Home.py
@@ -13,7 +13,7 @@ st.subheader(":blue[What is FreeStream?]")
 # Show footer
 st.markdown(footer, unsafe_allow_html=True)
 
-
+### Body content ###
 st.write(
     """
     FreeStream is a collection of chatbots that are tuned for specific use-cases.
@@ -21,6 +21,22 @@ st.write(
 )
 st.divider()
 st.subheader("What tools are currently available?")
+st.write(
+    """
+    ### :blue[Curie]:
+    
+    :orange[*This base chatbot is a more general purpose version of RAGbot. It's quicker and more focused on the user due to its system prompt and the lack of a retriever.*]
+    
+    Curie is perfect for venting your thoughts, getting constructive feedback on something you wrote, helping you make sense of things. Plus you don't have to upload any files. It's great for stuffing context into your questions when you use the Claude Haiku model. Specifically, it's been told to actively assist users in comprehending reality using their curiousity and critical thinking skills. It has a tendency to nudge you towards answers rather than giving them away, which is great for learning.
+    """
+)
+
+with st.expander(label=":violet[System Prompt:]", expanded=False):
+    st.markdown(
+        """
+        *You are a friendly AI chatbot designed to assist users in comprehending reality, exploring their curiosity, and practicing critical thinking skills. Your role is to guide users towards the right answers by providing thoughtful, well-reasoned responses. When faced with a question, decompose the problem into smaller, manageable parts and reason through each step systematically. This approach will help you provide comprehensive and accurate answers. Remember, your goal is to enhance learning and understanding, so only provide direct advice when explicitly asked to do so. Always strive to provide responses that are relevant, accurate, and contextually appropriate.*
+        """
+    )
 st.write(
     """
     ### :blue[RAGbot]:
@@ -49,27 +65,7 @@ with st.expander(label=":violet[RAGbot workflow:]", expanded=False):
         7. Question Answering:  The system meticulously answers your question, drawing knowledge exclusively from the retrieved content.
         """
     )
-
-
-st.write(
-    """
-    ### :blue[Curie]:
-    
-    :orange[*This base chatbot is a more general purpose version of RAGbot. It's quicker and more focused on the user due to its system prompt and the lack of a retriever.*]
-    
-    Curie is perfect for venting your thoughts, getting constructive feedback on something you wrote, helping you make sense of things. Plus you don't have to upload any files. It's great for stuffing context into your questions when you use the Claude Haiku model. Specifically, it's been told to actively assist users in comprehending reality using their curiousity and critical thinking skills. It has a tendency to nudge you towards answers rather than giving them away, which is great for learning.
-    """
-)
-
-with st.expander(label=":violet[System Prompt:]", expanded=False):
-    st.markdown(
-        """
-        *You are a friendly AI chatbot designed to assist users in comprehending reality, exploring their curiosity, and practicing critical thinking skills. Your role is to guide users towards the right answers by providing thoughtful, well-reasoned responses. When faced with a question, decompose the problem into smaller, manageable parts and reason through each step systematically. This approach will help you provide comprehensive and accurate answers. Remember, your goal is to enhance learning and understanding, so only provide direct advice when explicitly asked to do so. Always strive to provide responses that are relevant, accurate, and contextually appropriate.*
-        """
-    )
-
 st.divider()
-
 st.markdown(
     """
     #### References
@@ -79,5 +75,4 @@ st.markdown(
     * **[FreeStream's GitHub Repository](https://github.com/Daethyra/FreeStream)**    
     """
 )
-
 st.divider()
